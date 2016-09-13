beb5a4827dc68cd39b75fb88fa846d5aa8b520c8
dfsp-spirit <ts+code@rcmd.org>
2016-09-13 13:17:42 +0100

    fix distance check and line drawing

--- Way2Close/Assets/Scripts/CountScore.cs
+++ Way2Close/Assets/Scripts/CountScore.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CountScore : MonoBehaviour {
 
@@ -14,34 +15,83 @@ public class CountScore : MonoBehaviour {
     int layerMaskEnemies = 1 << 9;
     LineRenderer lineRenderer;
     GameObject player;
+    List<Vector3> myPoints;
 
     void Start () {
-        score = 0;
-        multiplier = 1;
-        lineRenderer = gameObject.GetComponent<LineRenderer>();
-        player = GameObject.FindWithTag("Player");
+        score = 0;        
+        //lineRenderer = Camera.main.GetComponent<LineRenderer>();
+        //lineRenderer.enabled = false;
+        player = GameObject.FindWithTag("Player");        
+
+        
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.SetColors(Color.white, Color.white);
+        lineRenderer.SetWidth(0.05F, 0.05F);
+        lineRenderer.SetVertexCount(2);
+        lineRenderer.sortingLayerName = "Front";
     }
 	
 
 	void Update () {
+        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        myPoints = new List<Vector3>();
+        //float t = Time.time;
+        //int i = 0;
+        //while (i < lengthOfLineRenderer)
+        //{
+        //    Vector3 pos = new Vector3(i * 0.5F, Mathf.Sin(i + t), 0);
+        //    lineRenderer.SetPosition(i, pos);
+        //    i++;
+        //}
+
+        multiplier = 1;
         Vector3 playerCenter = player.GetComponent<Renderer>().bounds.center;
 
-        Collider[] outerColliders = Physics.OverlapSphere(playerCenter, scoreOuterRadius);
-        foreach(Collider enemy in outerColliders)
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        string positionInfo = "";
+        foreach (GameObject enemy in enemies)
         {
-            lineRenderer.SetPosition(0, player.transform.position);
-            lineRenderer.SetPosition(1, enemy.transform.position);
-            lineRenderer.enabled = true;
+            positionInfo += " " + enemy.transform.position;
+            //lineRenderer.enabled = true;            
+            //lineRenderer.enabled = true;
+
+            Vector3 distance = player.transform.position - enemy.transform.position;
+            if(distance.sqrMagnitude < 5.0F)
+            {
+                multiplier++;
+                lineRenderer.enabled = true;
+                lineRenderer.SetPosition(0, player.transform.position);
+                lineRenderer.SetPosition(1, enemy.transform.position);
+                //Gizmos.DrawLine(player.transform.position, enemy.transform.position);
+            }
+            else
+            {
+                lineRenderer.enabled = false;
+            }
+            
         }
 
-        multiplier += outerColliders.Length;
-
-        Debug.Log("Player center at " + playerCenter.ToString() + ", " + outerColliders.Length.ToString() + " colliders in range " + scoreOuterRadius.ToString() + ".");
-        printEnemyPos();
+        Debug.Log("Player center at " + playerCenter.ToString() + ", " + (multiplier - 1).ToString() + " colliders in range.");
+        //printEnemyPos();
 
         score += multiplier * (Time.deltaTime * scorePerSecond);
         scoreText.text = "Score: " + score.ToString("n2");
         multiplierText.text = multiplier.ToString() + "x";
+        multiplierText.fontSize = 20 + (2 * multiplier);
+        multiplierText.color = Color.white;
+        if (multiplier >= 2)
+        {
+            multiplierText.color = Color.yellow;
+        }
+        if (multiplier >= 4)
+        {
+            multiplierText.color = Color.magenta;
+        }
+        if (multiplier >= 6)
+        {
+            multiplierText.color = Color.red;
+        }
     }
 
     void printEnemyPos()
@@ -53,9 +103,12 @@ public class CountScore : MonoBehaviour {
             positionInfo += " " + enemy.transform.position;
             //lineRenderer.SetPosition(0, player.transform.position);
             //lineRenderer.SetPosition(1, enemy.transform.position);
-            lineRenderer.SetPosition(0, new Vector3(0, 0));
-            lineRenderer.SetPosition(1, new Vector3(500, 500));
-            lineRenderer.enabled = true;
+            //lineRenderer.SetPosition(0, new Vector3(0, 0));
+            //lineRenderer.SetPosition(1, new Vector3(500, 500));
+            //lineRenderer.enabled = true;
+
+            Vector3 distance = player.transform.position - enemy.transform.position;
+            Debug.Log("Found enemy in distance " + distance.ToString() + ". Magnitude is " + distance.magnitude.ToString() + ". SqrMagnitude is " + distance.sqrMagnitude.ToString() + ".");
         }
         Debug.Log("Found " + enemies.Length.ToString() + " enemies:" + positionInfo);
     }
